8eda7d50b8c3c989642e60ef5efb09af591dc6a9
CaioAReis <Caio.almeida16.ca@gmail.com>
2020-05-12 22:16:41 -0300

    Imprementação das classes completa

--- src/POO/IFSRobot/AbsVacuumCleanerRobot.java
+++ src/POO/IFSRobot/AbsVacuumCleanerRobot.java
@@ -1,4 +1,32 @@
 package POO.IFSRobot;
 
-public class AbsVacuumCleanerRobot {
+public abstract class AbsVacuumCleanerRobot extends AbsBasicRobot implements IVacuumCleaner{
+    // ATRIBUTOS
+    private boolean switchedOn;
+
+    // CONSTRUTORES
+    public AbsVacuumCleanerRobot(Environment environment) {
+        super(environment);
+    }
+
+    // METODOS
+    public void turnOn() {
+        this.switchedOn = true;
+    }
+
+    public void turnOff() {
+        this.switchedOn = false;
+    }
+
+    public boolean isDirty() {
+        return environment.getValue(getCurrentPosition()) == 'S';
+    }
+
+    public void clear() {
+        this.environment.setValue(getCurrentPosition(),' ');
+    }
+
+    public boolean isSwitchedOn(){
+        return switchedOn;
+    }
 }
--- src/POO/IFSRobot/LeftRigthVacuumCleanerRobot.java
+++ src/POO/IFSRobot/LeftRigthVacuumCleanerRobot.java
@@ -1,4 +1,21 @@
 package POO.IFSRobot;
 
-public class LeftRigthVacuumCleanerRobot {
+public class LeftRigthVacuumCleanerRobot extends AbsVacuumCleanerRobot{
+    // CONSTRUTOR
+    public LeftRigthVacuumCleanerRobot(Environment environment) {
+        super(environment);
+    }
+    // METODOS
+    @Override
+    public Position move() {
+
+        if (currentPosition.getCol() + 1 < environment.getWidth()){
+            currentPosition.setCol(currentPosition.getCol() + 1);
+            return currentPosition;
+        }else {
+            currentPosition.setCol(0);
+            currentPosition.setRow(currentPosition.getRow() + 1);
+            return currentPosition;
+        }
+    }
 }
